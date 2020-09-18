e6bc39d630dd00e953b80ea5ef28368d7bde00c3
Horpschenzy <opeoluwa.lanre@gmail.com>
2020-09-18 12:19:39 +0100

    working on chat

--- app/Http/Controllers/MessageController.php
+++ app/Http/Controllers/MessageController.php
@@ -6,6 +6,7 @@ use Illuminate\Http\Request;
 use App\Models\Message;
 use App\User;
 use Auth;
+use App\Events\NewMessage;
 class MessageController extends Controller
 {
     public function get()
@@ -13,18 +14,25 @@ class MessageController extends Controller
         $contacts = User::whereHas('roles', function($q){
             $q->where('name','!=', 'SuperAdmin');
         })->where('id','!=',Auth::id())->get();
-        $unreadIds = Message::select(\DB::raw('`from` as sender_id, count(`from`) as messages_count'))
+        $unreadIds = Message::select(
+                                        \DB::raw('`from` as sender_id, count(`from`) as messages_count, `text` as convo'))
             ->where('to', auth()->id())
             ->where('read', false)
-            ->groupBy('from')
+            ->groupBy('from','text')
+            ->get();
+        $unreadIdss = Message::select(
+                                        \DB::raw('`from` as sender_id, count(`from`) as messages_count, `text` as convo'))
+            ->where('to', auth()->id())
+            ->groupBy('from','text')
             ->get();
 
         // add an unread key to each contact with the count of unread messages
-        $contacts = $contacts->map(function($contact) use ($unreadIds) {
+        $contacts = $contacts->map(function($contact) use ($unreadIds,$unreadIdss) {
             $contactUnread = $unreadIds->where('sender_id', $contact->id)->first();
+            $contactUnreads = $unreadIdss->where('sender_id', $contact->id)->first();
 
             $contact->unread = $contactUnread ? $contactUnread->messages_count : 0;
-
+            $contact->message = $contactUnreads ? $contactUnreads->convo : '';
             return $contact;
         });
 
@@ -48,5 +56,15 @@ class MessageController extends Controller
         ->get();
 
         return response()->json(['data'=>  $messages],200);
+    }
+    public function send(Request $request)
+    {
+        $message = Message::create([
+            'from' => auth()->id(),
+            'to' => $request->contact_id,
+            'text' => $request->text
+        ]);
+
+        return response()->json(['data'=>  $message],200);
     }
 }
